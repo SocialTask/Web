387ca0ff6eea0984972d85ef4950f79e73dce3a4
AgentitoE <agentee200@gmail.com>
2024-02-19 18:56:18 +0100

    Add 404 Page

---
+++ src/components/Feedback.jsx
@@ -0,0 +1,7 @@
+import pkg from '@typeform/embed-react'
+import '@fontsource-variable/onest';
+const { Widget } = pkg
+
+export default function MyPage() {
+    return <Widget id="u2zhsvmp0r3" style={{ width: '50%' }} className="my-form" />
+  }
